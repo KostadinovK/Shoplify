08affed202b071bf58b095c8108e22430d1afbd7
KostadinovK <kostadinovk@gmail.com>
2020-02-01 13:39:44 +0200

    Added init project structure

---
+++ Shoplify/Shoplify.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shoplify.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Shoplify/Shoplify.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shoplify.Services
+{
+    public class Class1
+    {
+    }
+}
