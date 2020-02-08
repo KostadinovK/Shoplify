a3e56816c84415b8b3485270f9da355bc5b11f8a
KostadinovK <kostadinovk@gmail.com>
2020-02-08 13:43:31 +0200

    Added ISeeder interface

---
+++ Shoplify/Shoplify.Data/Seeding/ISeeder.cs
@@ -0,0 +1,12 @@
+﻿namespace Shoplify.Data.Seeding
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Shoplify.Web.Data;
+
+    public interface ISeeder
+    {
+        Task<int> SeedAsync(ShoplifyDbContext context, IServiceProvider serviceProvider);
+    }
+}
