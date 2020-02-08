381b734730da3253f1eb61d83292117f0179951e
KostadinovK <kostadinovk@gmail.com>
2020-02-08 11:02:31 +0200

    Now password resetting views are less ugly

---
+++ Shoplify/Shoplify.Web/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -0,0 +1,95 @@
+﻿using Shoplify.Common;
+
+namespace Shoplify.Web.Areas.Identity.Pages.Account
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.WebUtilities;
+    using Shoplify.Domain;
+
+    [AllowAnonymous]
+    public class ResetPasswordModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+
+        public ResetPasswordModel(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [StringLength(AttributesConstraints.UsernameMaxLength, ErrorMessage = AttributesErrorMessages.UsernameInvalidLength, MinimumLength = AttributesConstraints.UsernameMinLength)]
+            public string Username { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(AttributesConstraints.PasswordMaxLength, ErrorMessage = AttributesErrorMessages.PasswordInvalidLength, MinimumLength = AttributesConstraints.PasswordMinLength)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = AttributesErrorMessages.ConfirmPasswordInvalid)]
+            public string ConfirmPassword { get; set; }
+
+            public string Code { get; set; }
+        }
+
+        public IActionResult OnGet(string code = null)
+        {
+            if (code == null)
+            {
+                return BadRequest("A code must be supplied for password reset.");
+            }
+            else
+            {
+                Input = new InputModel
+                {
+                    Code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code))
+                };
+                return Page();
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await userManager.FindByNameAsync(Input.Username);
+            if (user == null || user.Email != Input.Email)
+            {
+                // Don't reveal that the user does not exist
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            var result = await userManager.ResetPasswordAsync(user, Input.Code, Input.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return Page();
+        }
+    }
+}
