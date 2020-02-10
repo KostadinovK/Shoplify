f72db2d23e404efb9f1a1332f07612a8d548397e
KostadinovK <kostadinovk@gmail.com>
2020-02-10 13:46:57 +0200

    Added unit tests for all methods in SubCategoryService for now

--- Shoplify/Shoplify.Tests/ServicesTests/SubCategoryServiceTests.cs
+++ Shoplify/Shoplify.Tests/ServicesTests/SubCategoryServiceTests.cs
@@ -116,5 +116,160 @@ namespace Shoplify.Tests.ServicesTests
 
             Assert.AreEqual(expectedCategoryCount, actualCategoryCount);
         }
+
+        [Test]
+        public async Task ContainsByIdAsync_WithValidId_ShouldReturnTrue()
+        {
+            var subCategoryName = "test";
+
+            var subCategory = new SubCategory()
+            {
+                Name = subCategoryName,
+                CategoryId = "test",
+            };
+
+            await context.SubCategories.AddAsync(subCategory);
+            await context.SaveChangesAsync();
+
+            var subCategoryFromDb = await context.SubCategories.FirstOrDefaultAsync(s => s.Name == subCategoryName);
+
+            var result = await service.ContainsByIdAsync(subCategoryFromDb.Id.ToString());
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task ContainsByIdAsync_WithInvalidId_ShouldReturnFalse()
+        {
+            var result = await service.ContainsByIdAsync("invalidId");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task GetByIdAsync_WithInvalidId_ShouldThrowArgumentException()
+        {
+            Assert.ThrowsAsync<ArgumentException>(async () => await service.GetByIdAsync("invalidId"));
+        }
+
+        [Test]
+        public async Task GetByIdAsync_WithValidId_ShouldReturnCorrectly()
+        {
+            var subCategoryName = "test";
+
+            var subCategory = new SubCategory()
+            {
+                Name = subCategoryName,
+                CategoryId = "test",
+            };
+
+            await context.SubCategories.AddAsync(subCategory);
+            await context.SaveChangesAsync();
+
+            var subCategoryFromDb = await context.SubCategories.FirstOrDefaultAsync(s => s.Name == subCategoryName);
+
+            var actualCategory = await service.GetByIdAsync(subCategoryFromDb.Id.ToString());
+
+            var expectedCategory = new SubCategoryServiceModel()
+            {
+                Name = subCategory.Name,
+                CategoryId = subCategory.CategoryId,
+                Id = subCategory.Id,
+            };
+
+            AssertEx.PropertyValuesAreEquals(actualCategory, expectedCategory);
+        }
+
+        [Test]
+        public async Task GetByNameAsync_WithInvalidName_ShouldThrowArgumentException()
+        {
+            Assert.ThrowsAsync<ArgumentException>(async () => await service.GetByNameAsync("invalidName"));
+        }
+
+        [Test]
+        public async Task GetByNameAsync_WithValidName_ShouldReturnCorrectly()
+        {
+            var subCategoryName = "test";
+
+            var subCategory = new SubCategory()
+            {
+                Name = subCategoryName,
+                CategoryId = "test",
+            };
+
+            await context.SubCategories.AddAsync(subCategory);
+            await context.SaveChangesAsync();
+
+            var actualCategory = await service.GetByNameAsync(subCategory.Name);
+
+            var expectedCategory = new SubCategoryServiceModel()
+            {
+                Name = subCategory.Name,
+                CategoryId = subCategory.CategoryId,
+                Id = subCategory.Id,
+            };
+
+            AssertEx.PropertyValuesAreEquals(actualCategory, expectedCategory);
+        }
+
+        [Test]
+        public async Task GetAllByCategoryId_ShouldReturnCorrectly()
+        {
+            var categoryName = "test";
+
+            var category = new Category
+            {
+                Name = categoryName,
+            };
+
+            await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+
+            var subCategoryOne = new SubCategory
+            {
+                Name = "test",
+                CategoryId = category.Id,
+            };
+
+            var subCategoryTwo = new SubCategory
+            {
+                Name = "test2",
+                CategoryId = category.Id,
+            };
+
+            var subCategoryThree = new SubCategory
+            {
+                Name = "test3",
+                CategoryId = "anotherId",
+            };
+
+            await context.SubCategories.AddAsync(subCategoryOne);
+            await context.SubCategories.AddAsync(subCategoryTwo);
+            await context.SubCategories.AddAsync(subCategoryThree);
+            await context.SaveChangesAsync();
+
+            var subCategories = service.GetAllByCategoryId(category.Id).ToList();
+
+            var expectedSubCategoryOne = new SubCategoryServiceModel()
+            {
+                Name = subCategoryOne.Name,
+                CategoryId = subCategoryOne.CategoryId,
+                Id = subCategoryOne.Id,
+            };
+
+            var expectedSubCategoryTwo = new SubCategoryServiceModel
+            {
+                Name = subCategoryTwo.Name,
+                CategoryId = subCategoryTwo.CategoryId,
+                Id = subCategoryTwo.Id,
+            };
+
+            var expectedCategoriesCount = 2;
+            var actualCategoriesCount = subCategories.Count;
+
+            Assert.AreEqual(expectedCategoriesCount, actualCategoriesCount);
+            AssertEx.PropertyValuesAreEquals(subCategories[0], expectedSubCategoryOne);
+            AssertEx.PropertyValuesAreEquals(subCategories[1], expectedSubCategoryTwo);
+        }
     }
 }
