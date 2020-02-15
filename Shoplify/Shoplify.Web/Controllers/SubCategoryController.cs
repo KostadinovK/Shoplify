e654d857ab1981e489f27af3e2cef9ece70316d7
KostadinovK <kostadinovk@gmail.com>
2020-02-15 10:40:58 +0200

    Added functionality for subCategories fetching based on categoryId

---
+++ Shoplify/Shoplify.Web/Controllers/SubCategoryController.cs
@@ -0,0 +1,26 @@
+﻿namespace Shoplify.Web.Controllers
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Shoplify.Services.Interfaces;
+
+    [Authorize]
+    public class SubCategoryController : Controller
+    {
+        private ISubCategoryService subCategoryService;
+
+        public SubCategoryController(ISubCategoryService subCategoryService)
+        {
+            this.subCategoryService = subCategoryService;
+        }
+
+        public IActionResult GetByCategoryId(string categoryId)
+        {
+            var subCategories = subCategoryService.GetAllByCategoryId(categoryId).ToList();
+
+            return Json(subCategories);
+        }
+    }
+}
---
+++ Shoplify/Shoplify.Web/wwwroot/js/fetchSubCategories.js
@@ -0,0 +1,13 @@
+﻿function displaySubCategories(element) {
+    let url = `/SubCategory/GetByCategoryId`;
+
+    let subCategoriesSelectMenu = document.getElementById("subCategorySelectMenu");
+
+    let selectedCategoryId = element.value;
+    url += `?categoryId=${selectedCategoryId}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(subCategories => console.log(subCategories))
+        .catch(err => console.log(err));
+}
