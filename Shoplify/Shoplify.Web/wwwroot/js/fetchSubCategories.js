762d9be7ae0ba7a601ccf509e7bb6257b74fa726
KostadinovK <kostadinovk@gmail.com>
2020-02-15 13:17:42 +0200

    Refactored fetchSubCategories.js

--- Shoplify/Shoplify.Web/wwwroot/js/fetchSubCategories.js
+++ Shoplify/Shoplify.Web/wwwroot/js/fetchSubCategories.js
@@ -1,4 +1,4 @@
-﻿function displaySubCategories(element) {
+﻿function getSubCategories(element) {
     let url = `/SubCategory/GetByCategoryId`;
 
     let subCategoriesSelectMenu = document.getElementById("subCategorySelectMenu");
@@ -9,39 +9,41 @@
 
     fetch(url)
         .then(response => response.json())
-        .then((subCategories) => {
-
-            while (subCategoriesSelectMenu.firstChild) {
-                subCategoriesSelectMenu.removeChild(subCategoriesSelectMenu.firstChild);
-            }
-
-            let initial = document.createElement("option");
-
-            initial.value = '0';
-            initial.text = 'Select SubCategory';
-            initial.disabled = true;
-            initial.selected = true;
-
-            subCategoriesSelectMenu.appendChild(initial);
-
-            for (let subCategory of subCategories) {
-
-                let subCategoryElement = document.createElement("option");
-
-                subCategoryElement.value = subCategory.id;
-                subCategoryElement.text = subCategory.name;
-
-                subCategoriesSelectMenu.appendChild(subCategoryElement);
-            }
-
-            subCategoryDiv.innerHTML = '';
-            subCategoryDiv.appendChild(subCategoriesSelectMenu);
-
-            subCategoriesSelectMenu.classList.add("nice-select");
-            subCategoriesSelectMenu.classList.add("current");
-            subCategoriesSelectMenu.style.color = "#777777";
-
-            subCategoriesSelectMenu.style.display = 'block';
-        })
+        .then(subCategories => displaySubCategories(subCategories, subCategoriesSelectMenu, subCategoryDiv))
         .catch(err => console.log(err));
+}
+
+function displaySubCategories(subCategories, selectMenu, subCategoryDiv) {
+    while (selectMenu.firstChild) {
+        selectMenu.removeChild(selectMenu.firstChild);
+    }
+
+    let initial = document.createElement("option");
+
+    initial.value = '0';
+    initial.text = 'Select SubCategory';
+    initial.disabled = true;
+    initial.selected = true;
+
+    selectMenu.appendChild(initial);
+
+    for (let subCategory of subCategories) {
+
+        let subCategoryElement = document.createElement("option");
+        subCategoryElement.value = subCategory.id;
+        subCategoryElement.text = subCategory.name;
+
+        subCategoryElement.style.color = "#777777";
+
+        selectMenu.appendChild(subCategoryElement);
+    }
+
+    subCategoryDiv.innerHTML = '';
+    subCategoryDiv.appendChild(selectMenu);
+
+    selectMenu.classList.add("nice-select");
+    selectMenu.classList.add("current");
+    selectMenu.style.color = "#777777";
+
+    selectMenu.style.display = 'block';
 }
