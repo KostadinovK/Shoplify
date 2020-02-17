df2ca3a35bce1eb5465d97f62c830d5995b11b7b
KostadinovK <kostadinovk@gmail.com>
2020-02-17 10:25:18 +0200

    Added unit test for advertisement create

---
+++ Shoplify/Shoplify.Tests/ServicesTests/AdvertisementServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Shoplify.Domain.Enums;
+
+namespace Shoplify.Tests.ServicesTests
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using NUnit.Framework;
+    using Shoplify.Services.Implementations;
+    using Shoplify.Services.Interfaces;
+    using Shoplify.Services.Models;
+    using Shoplify.Web.Data;
+
+    [TestFixture]
+    public class AdvertisementServiceTests
+    {
+        private ShoplifyDbContext context;
+        private IAdvertisementService service;
+        private IFormFile mockedFile;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ShoplifyDbContext>()
+                .UseInMemoryDatabase(databaseName: "ads")
+                .Options;
+
+            this.context = new ShoplifyDbContext(options);
+
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            var moqCloudinaryService = new Mock<ICloudinaryService>();
+            var moqIFormFile = new Mock<IFormFile>();
+
+            this.mockedFile = moqIFormFile.Object;
+
+            moqCloudinaryService.Setup(x => x.UploadPictureAsync(moqIFormFile.Object, "FileName"))
+                .ReturnsAsync("http://test.com");
+
+            this.service = new AdvertisementService(context, moqCloudinaryService.Object);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await context.DisposeAsync();
+        }
+
+        [Test]
+        public async Task CreateAsync_WithValidData_ShouldCreateSuccessfully()
+        {
+            var advertisement = new AdvertisementServiceModel()
+            {
+                Name = "OnePlus 7 Pro",
+                Description = "cool phone for everyday use, excellent performance",
+                Price = 800,
+                Condition = ProductCondition.New,
+                CategoryId = "testCategoryId",
+                SubCategoryId = "testCategoryId",
+                Images = new List<IFormFile>
+                {
+                    mockedFile
+                },
+                TownId = "testTownId",
+                Address = "str nqkoq",
+                Number = "telefonce"
+            };
+
+            await service.CreateAsync(advertisement);
+
+            var actualCategoryCount = context.Advertisements.Count();
+            var expectedCategoryCount = 1;
+
+            Assert.AreEqual(expectedCategoryCount, actualCategoryCount);
+        }
+    }
+}
