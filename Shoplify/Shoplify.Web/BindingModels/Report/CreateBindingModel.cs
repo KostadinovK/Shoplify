07c23641dd35a2a6aef59ec1e2d19b3ce9984413
KostadinovK <kostadinovk@gmail.com>
2020-03-01 12:54:49 +0200

    Added report create page

---
+++ Shoplify/Shoplify.Web/BindingModels/Report/CreateBindingModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shoplify.Web.BindingModels.Report
+{
+    using System.ComponentModel.DataAnnotations;
+    using Shoplify.Common;
+
+    public class CreateBindingModel
+    {
+        public string ReportingUserId { get; set; }
+
+        public string ReportedUserId { get; set; }
+
+        public string ReportedAdvertisementId { get; set; }
+
+        [MaxLength(AttributesConstraints.ReportDescriptionMaxLength)]
+        public string Description { get; set; }
+    }
+}
---
+++ Shoplify/Shoplify.Web/Controllers/ReportController.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Claims;
+using Shoplify.Web.BindingModels.Report;
+
+namespace Shoplify.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Shoplify.Services.Interfaces;
+
+    [Authorize]
+    [AutoValidateAntiforgeryToken]
+    public class ReportController : Controller
+    {
+        private readonly IAdvertisementService advertisementService;
+
+        public ReportController(IAdvertisementService advertisementService)
+        {
+            this.advertisementService = advertisementService;
+        }
+
+        public async Task<IActionResult> Create(string adId)
+        {
+            var ad = await advertisementService.GetByIdAsync(adId);
+
+            var model = new CreateBindingModel
+            {
+                ReportedUserId = ad.UserId,
+                ReportedAdvertisementId = ad.Id,
+                ReportingUserId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBindingModel input)
+        {
+            return View();
+        }
+    }
+}
