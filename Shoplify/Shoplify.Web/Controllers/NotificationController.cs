ead2849c38f982be8cbddf9f4bbf78f114c366a5
KostadinovK <kostadinovk@gmail.com>
2020-03-10 15:32:26 +0200

    Unread notifications count are visualized in user navbar

---
+++ Shoplify/Shoplify.Web/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿namespace Shoplify.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Shoplify.Services.Interfaces;
+
+    [Authorize]
+    public class NotificationController : Controller
+    {
+        private readonly INotificationService notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+
+        public async Task<IActionResult> GetAllCount(string userId)
+        {
+            var notificationsCount = await notificationService.GetAllUnReadByUserIdCountAsync(userId);
+
+            return Json(notificationsCount);
+        }
+    }
+}
---
+++ Shoplify/Shoplify.Web/wwwroot/js/notifications.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded",
+    () => {
+        let notificationCounter = document.getElementById("notificationsCount");
+        let userId = document.getElementById("userId").value;
+
+        fetch(`/Notification/GetAllCount?userId=${userId}`)
+            .then(response => response.json())
+            .then(notificationsCount => notificationCounter.textContent = notificationsCount)
+            .catch(err => console.log(err));
+    });
