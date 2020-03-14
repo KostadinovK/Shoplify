e11ca1ec02c03f7f5aed0ee72514866a256261f7
KostadinovK <kostadinovk@gmail.com>
2020-03-14 10:17:06 +0200

    Implemented conversation service

--- Shoplify/Shoplify.Services/Implementations/ConversationService.cs
+++ Shoplify/Shoplify.Services/Implementations/ConversationService.cs
@@ -2,31 +2,111 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
+    using Microsoft.EntityFrameworkCore;
+    using Shoplify.Domain;
     using Shoplify.Services.Interfaces;
     using Shoplify.Services.Models.Conversation;
+    using Shoplify.Web.Data;
 
     public class ConversationService : IConversationService
     {
-        public Task<ConversationServiceModel> CreateConversationAsync(string firstUserId, string secondUserId, string adId)
+        private ShoplifyDbContext context;
+
+        public ConversationService(ShoplifyDbContext context)
         {
-            throw new NotImplementedException();
+            this.context = context;
         }
 
-        public Task<bool> MarkConversationAsReadAsync(string conversationId, string userId)
+        public async Task<ConversationServiceModel> CreateConversationAsync(string firstUserId, string secondUserId, string adId)
         {
-            throw new NotImplementedException();
+            var conversation = new Conversation
+            {
+                FirstUserId = firstUserId,
+                SecondUserId = secondUserId,
+                AdvertisementId = adId,
+                IsReadByFirstUser = false,
+                IsReadBySecondUser = false,
+                StartedOn = DateTime.UtcNow
+            };
+
+            await context.AddAsync(conversation);
+            await context.SaveChangesAsync();
+
+            return new ConversationServiceModel
+            {
+                Id = conversation.Id,
+                FirstUserId = conversation.FirstUserId,
+                SecondUserId = conversation.SecondUserId,
+                AdvertisementId = conversation.AdvertisementId,
+                IsReadByFirstUser = conversation.IsReadByFirstUser,
+                IsReadBySecondUser = conversation.IsReadBySecondUser,
+                StartedOn = conversation.StartedOn
+            };
         }
 
-        public Task<int> GetAllUnReadByUserIdCountAsync(string userId)
+        public async Task<bool> MarkConversationAsReadAsync(string conversationId, string userId)
         {
-            throw new NotImplementedException();
+            if (!context.Conversation.Any(c => c.Id == conversationId))
+            {
+                return false;
+            }
+
+            var conversation = context.Conversation.SingleOrDefault(c => c.Id == conversationId);
+
+            if (conversation.FirstUserId == userId)
+            {
+                conversation.IsReadByFirstUser = true;
+            }
+            else if (conversation.SecondUserId == userId)
+            {
+                conversation.IsReadBySecondUser = true;
+            }else
+            {
+                return false;
+            }
+
+            context.Conversation.Update(conversation);
+
+            await context.SaveChangesAsync();
+
+            return true;
         }
 
-        public Task<IEnumerable<ConversationServiceModel>> GetAllByUserIdAsync(string userId)
+        public async Task<int> GetAllUnReadByUserIdCountAsync(string userId)
         {
-            throw new NotImplementedException();
+            var conversations = await context.Conversation.Where(c =>
+                (c.FirstUserId == userId && !c.IsReadByFirstUser) &&
+                (c.SecondUserId == userId && !c.IsReadBySecondUser)).ToListAsync();
+
+            return conversations.Count;
+        }
+
+        public async Task<IEnumerable<ConversationServiceModel>> GetAllByUserIdAsync(string userId)
+        {
+            var conversations = await context.Conversation.Where(c =>
+                c.FirstUserId == userId &&
+                c.SecondUserId == userId).ToListAsync();
+
+            var result = new List<ConversationServiceModel>();
+
+            foreach (var conversation in conversations)
+            {
+                result.Add(new ConversationServiceModel
+                {
+                    Id = conversation.Id,
+                    FirstUserId = conversation.FirstUserId,
+                    SecondUserId = conversation.SecondUserId,
+                    AdvertisementId = conversation.AdvertisementId,
+                    IsReadByFirstUser = conversation.IsReadByFirstUser,
+                    IsReadBySecondUser = conversation.IsReadBySecondUser,
+                    StartedOn = conversation.StartedOn
+                });
+            }
+
+            return result;
         }
     }
 }
