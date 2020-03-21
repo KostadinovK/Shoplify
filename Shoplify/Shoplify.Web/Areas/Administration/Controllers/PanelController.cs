33b8cea913a3d17aab128523d51a6dc9b454d68d
KostadinovK <kostadinovk@gmail.com>
2020-03-21 13:53:47 +0200

    Added all ads count in admin panel and also removed button for categories

--- Shoplify/Shoplify.Web/Areas/Administration/Controllers/PanelController.cs
+++ Shoplify/Shoplify.Web/Areas/Administration/Controllers/PanelController.cs
@@ -1,32 +1,35 @@
-﻿using Shoplify.Services.Interfaces;
-using Shoplify.Web.Areas.Administration.ViewModels.Panel;
-
-namespace Shoplify.Web.Areas.Administration.Controllers
+﻿namespace Shoplify.Web.Areas.Administration.Controllers
 {
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Shoplify.Common;
+    using Shoplify.Services.Interfaces;
+    using Shoplify.Web.Areas.Administration.ViewModels.Panel;
 
     [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     [Area("Administration")]
     public class PanelController : Controller
     {
         private readonly IUserService userService;
+        private readonly IAdvertisementService advertisementService;
 
-        public PanelController(IUserService userService)
+        public PanelController(IUserService userService, IAdvertisementService advertisementService)
         {
             this.userService = userService;
+            this.advertisementService = advertisementService;
         }
 
         public async Task<IActionResult> Index()
         {
             var usersCount = await userService.GetAllUserCountWithoutAdminAsync();
+            var adsCount = await advertisementService.GetAllAdsCountAsync();
 
             var viewModel = new PanelViewModel
             {
                 UsersCount = usersCount,
+                AdsCount = adsCount
             };
 
             return View(viewModel);
