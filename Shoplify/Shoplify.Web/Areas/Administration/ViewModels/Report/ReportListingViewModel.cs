6e5ba223a0e9c0ae2653ec4bd155214e5d6e4597
KostadinovK <kostadinovk@gmail.com>
2020-03-23 11:45:39 +0200

    Added Report ViewModel and Report Listing page ViewModel

---
+++ Shoplify/Shoplify.Web/Areas/Administration/ViewModels/Report/ReportListingViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shoplify.Web.Areas.Administration.ViewModels.Report
+{
+    using System.Collections.Generic;
+
+    public class ReportListingViewModel
+    {
+        public ICollection<ReportViewModel> Reports { get; set; } = new List<ReportViewModel>();
+
+        public int TotalReportsCount { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int LastPage { get; set; }
+    }
+}
---
+++ Shoplify/Shoplify.Web/Areas/Administration/ViewModels/Report/ReportViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Shoplify.Web.Areas.Administration.ViewModels.Report
+{
+    public class ReportViewModel
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string CreatedOn { get; set; }
+
+        public string AdvertisementId { get; set; }
+
+        public string AdvertisementName { get; set; }
+
+        public string ReportedUserId { get; set; }
+
+        public string ReportedUserName { get; set; }
+
+        public string ReportingUserId { get; set; }
+
+        public string ReportingUserName { get; set; }
+    }
+}
