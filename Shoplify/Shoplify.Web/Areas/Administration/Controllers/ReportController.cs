96544adbda522418b49fe7718cadbd059402ad27
KostadinovK <kostadinovk@gmail.com>
2020-03-23 11:57:14 +0200

    Added reports page view

---
+++ Shoplify/Shoplify.Web/Areas/Administration/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿namespace Shoplify.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Shoplify.Common;
+
+    [Area("Administration")]
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class ReportController : Controller
+    {
+    }
+}
